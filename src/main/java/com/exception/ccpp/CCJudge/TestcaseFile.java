91274d460faf8748df7b51128349a82959e349af
Glensh <glenshc@gmail.com>
2025-12-05 23:56:32 +0800

    TestcaseFile and CrypticWriter

--- src/main/java/com/exception/ccpp/CCJudge/TestcaseFile.java
+++ src/main/java/com/exception/ccpp/CCJudge/TestcaseFile.java
@@ -1,8 +1,12 @@
 package com.exception.ccpp.CCJudge;
 
 import com.exception.ccpp.FileManagement.CCFile;
+import com.exception.ccpp.FileManagement.CrypticWriter;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TestcaseFile extends CCFile {
     private String[][] inputs;
@@ -16,17 +20,47 @@ public class TestcaseFile extends CCFile {
         super(path);
     }
 
+    public TestcaseFile(String filepath, String[][] inputs, String[] expected_outputs) {
+        super(filepath);
+        this.inputs = inputs;
+        this.expected_outputs = expected_outputs;
+        writeOut();
+    }
+
+    String[][] getInputs() {
+        return inputs;
+    }
+    public String[] getExpectedOutputs() {
+        return expected_outputs;
+    }
+
+
     @Override
     protected void load() {
-        // TODO
+        try {
+            List<Object> retrievedData = CrypticWriter.readEncryptedData(path);
+            if (retrievedData.size() > 0) {
+                inputs = (String[][]) retrievedData.get(1);
+                expected_outputs = (String[]) retrievedData.get(0);
+            }
+        } catch (Exception e) {
+            System.err.println("TestcaseFile.load() error loading " + path);
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void writeOut() {
-        // TODO
-    }
-
-    public void use() {
-        // TODO
+        {
+            List<Object> data = new ArrayList<>();
+            data.add(inputs);
+            data.add(expected_outputs);
+            try {
+                CrypticWriter.writeEncryptedData(data, path);
+            } catch (Exception e) {
+                System.err.println("TestcaseFile.writeOut() error writing " + path);
+                e.printStackTrace();
+            }
+        }
     }
 }
