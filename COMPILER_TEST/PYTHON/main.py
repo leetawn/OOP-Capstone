f7d425d69cd6ad442ee204f2b94e2ef78472973e
etawn <univaltacc6969@gmail.com>
2025-12-07 02:12:36 +0800

    python fix ?

--- COMPILER_TEST/PYTHON/main.py
+++ COMPILER_TEST/PYTHON/main.py
@@ -1,7 +1,2 @@
-x = input("Enter a number:          ")
-x = int(x)
-y = int(input("Enter a number y: "))
-res = x + y
-
-print(f"Sum: {res}")
-print("DIDDY!!!", end="")
+for i in range(1000000):
+    print(i)
