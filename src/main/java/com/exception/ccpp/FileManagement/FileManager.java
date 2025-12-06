f1bce41dcdaeb0c53d9595fbd216986e27d16d26
Glensh <glenshc@gmail.com>
2025-12-07 01:26:53 +0800

    c/cpp shouldn't compile header files idiot

--- src/main/java/com/exception/ccpp/FileManagement/FileManager.java
+++ src/main/java/com/exception/ccpp/FileManagement/FileManager.java
@@ -11,8 +11,8 @@ public class FileManager {
     public static final Set<String> IGNORED_FOLDERS = Set.of(".git", ".svn", ".vscode", "target", "out", "bin", "vendor", "System32", "Windows");
     public static final Set<String> ALL_ALLOWED_EXTENSIONS = Set.of(".c", ".cpp", ".h", ".hpp",".java",".py");
     public static final Set<String> JAVA_ALLOWED_EXTENSIONS = Set.of(".java");
-    public static final Set<String> CPP_ALLOWED_EXTENSIONS  = Set.of(".cpp", ".h", ".hpp");
-    public static final Set<String> C_ALLOWED_EXTENSIONS    = Set.of(".c", ".h");
+    public static final Set<String> CPP_ALLOWED_EXTENSIONS  = Set.of(".cpp");
+    public static final Set<String> C_ALLOWED_EXTENSIONS    = Set.of(".c");
     public static final Set<String> PY_ALLOWED_EXTENSIONS   = Set.of(".py");
     private static FileManager instance;
 
